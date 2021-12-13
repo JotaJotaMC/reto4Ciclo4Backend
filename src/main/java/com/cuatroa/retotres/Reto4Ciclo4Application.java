607d43305d98694f615ea0ee040d7df9e7c6f6dd
John Jairo Mogollón Castillo <33044288+jotajotamcastillo@users.noreply.github.com>
2021-12-12 20:29:39 -0500

    BackenReto4

---
+++ src/main/java/com/cuatroa/retotres/Reto4Ciclo4Application.java
@@ -0,0 +1,44 @@
+package com.cuatroa.retotres;
+import com.cuatroa.retotres.model.Product;
+import com.cuatroa.retotres.model.Order;
+import com.cuatroa.retotres.model.User;
+import com.cuatroa.retotres.repository.crud.ProductCrudRepository;
+import com.cuatroa.retotres.repository.crud.OrderCrudRepository;
+import com.cuatroa.retotres.repository.crud.UserCrudRepository;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jotajotamcastillo
+ */
+
+@SpringBootApplication
+public class Reto4Ciclo4Application implements CommandLineRunner {
+
+    @Autowired
+    private ProductCrudRepository interfaceVegetales;
+    @Autowired
+    private UserCrudRepository interfaceUser;
+    @Autowired
+    private OrderCrudRepository interfaceOrder;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reto4Ciclo4Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        interfaceVegetales.deleteAll();
+        interfaceUser.deleteAll();
+        interfaceOrder.deleteAll(); 
+
+    }
+}
